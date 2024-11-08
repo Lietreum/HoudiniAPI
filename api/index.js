99a0490055559176d7ef66d9736a337d37ea7db4
Lietreum <mirsieolien@gmail.com>
2024-11-08 09:15:42 +0700

    re

--- api/index.js
+++ api/index.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({
+  origin: ['https://houdini-mauve.vercel.app', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
